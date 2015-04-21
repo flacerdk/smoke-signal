0f427001692fae60321f0a7428b265d815f1fe33
Felipe Lacerda <fegolac@gmail.com>
2015-04-21 23:38:11 +0200

    add Atom support

--- smoke_signal/fetch_feed.py
+++ smoke_signal/fetch_feed.py
@@ -1,7 +1,9 @@
 import urllib2
-import xml.etree.ElementTree as etree
+from lxml import etree
 from smoke_signal import db, app
 from smoke_signal.database.models import Feed, Entry
+
+NSMAP = {"atom": "http://www.w3.org/2005/Atom"}
 
 def read_feed(feed):
     feed_id = feed.id
@@ -9,6 +11,12 @@ def read_feed(feed):
     rss = urllib2.urlopen(url)
     tree = etree.parse(rss)
     root = tree.getroot()
+    if root.nsmap[None] == NSMAP["atom"]:
+        read_atom(feed_id, root)
+    else:
+        read_rss(feed_id, root)
+
+def read_rss(feed_id, root):
     entries = root.find('channel').findall('item')
     for e in entries:
         attributes = {'feed_id': feed_id}
@@ -22,6 +30,27 @@ def read_feed(feed):
             attributes[t] = content
         if db.session.query(Entry).filter(Entry.guid == attributes['guid']).all() == []:
             entry = Entry(**attributes)
+            with app.app_context():
+                db.session.add(entry)
+                db.session.commit()
+
+def read_atom(feed_id, root):
+    ns = "{" + NSMAP["atom"] + "}"
+    entries = root.findall("{ns}entry".format(ns=ns))
+    print entries
+    for e in entries:
+        attributes = {'feed_id': feed_id}
+        tags = ['title', 'id', 'content']
+        for t in tags:
+            attr = e.find(("{ns}" + t).format(ns=ns))
+            if attr != None:
+                content = attr.text
+            else:
+                content = ""
+            attributes[t] = content
+        if db.session.query(Entry).filter(Entry.guid == attributes['id']).all() == []:
+            entry = Entry(attributes['title'], attributes['id'],
+                          attributes['content'], attributes['feed_id'])
             with app.app_context():
                 db.session.add(entry)
                 db.session.commit()
