ce25b972e33b6d730ea38c9d7e7635828bb2a6f9
Felipe Lacerda <fegolac@gmail.com>
2015-04-22 20:37:51 +0200

    Decouple refreshing entries and showing entries
    Now you have to hit "refresh" to get new entries. Useful for now, but I'll
    have to figure out how to make it less annoying later.

--- smoke_signal/static/show_entries.js
+++ smoke_signal/static/show_entries.js
@@ -1,18 +1,40 @@
-$(function () {
-    $('li.feed').bind('click', function(event) {
-        $.getJSON($SCRIPT_ROOT + '/_show_entries', {
-            id: event.target.id
-        }, function(data) {
-            $('#entries').empty();
-            data.map(function(entry) {
-                entryTitle = document.createElement('p');
-                entryTitle.appendChild(document.createTextNode(entry.title));
-                $('#entries').append(entryTitle);
-                entryText = document.createElement('p');
-                entryText.appendChild(document.createTextNode(entry.text));
-                $('#entries').append(entryText);
-            });
-        });
+function showEntries(feedId) {
+    $('#entries').empty();
+    if (feedId == -1) {
+        entryText = document.createElement('p');
+        text = document.createTextNode("Nothing to show!");
+        entryText.appendChild(text);
+        $('#entries').append(entryText);
         return false;
+    }
+
+    $.getJSON($SCRIPT_ROOT + '/_show_entries', {
+        id: feedId
+    }, function(data) {
+        $('#entries').attr('feedId', feedId);
+        data.map(function(entry) {
+            entryTitle = document.createElement('p');
+            entryTitle.appendChild(document.createTextNode(entry.title));
+            $('#entries').append(entryTitle);
+            entryText = document.createElement('p');
+            entryText.appendChild(document.createTextNode(entry.text));
+            $('#entries').append(entryText);
+        });
+    });
+    return false;
+}
+
+$(document).ready(function () {
+    $('li.feed').bind('click', function(event) {
+        showEntries(event.target.id);
+    });
+    $('button#refresh').bind('click', function(event) {
+        var feedId = $('#entries').attr('feedId');
+        if (typeof feedId == "undefined")
+            feedId = -1;
+        $.getJSON($SCRIPT_ROOT + '/_refresh_entries', {
+            id: feedId
+        });
+        showEntries(feedId);
     });
 });
