6fd2ef21a1c8a7e2a5ff148fb75e59678e6db319
Felipe Lacerda <fegolac@gmail.com>
2016-10-13 21:31:26 +0200

    Add some basic error handling

--- smoke_signal/views.py
+++ smoke_signal/views.py
@@ -1,7 +1,8 @@
-from flask import request, render_template, g, Blueprint
+from flask import request, render_template, g, Blueprint, redirect, url_for
 from smoke_signal.database.models import Feed, Entry
 from smoke_signal.parse import parse_feed
 import json
+import sqlalchemy.orm.exc
 
 feed_view = Blueprint("feed_view", __name__,
                       template_folder="templates")
@@ -13,18 +14,21 @@ def show_feeds():
     return render_template('show_feeds.html', feeds=feeds)
 
 
-@feed_view.route('/feeds/<feed_id>')
+@feed_view.route('/feeds/<int:feed_id>')
 def show_entries(feed_id):
-    # feed_id is non-negative iff feed could be fetched
-    if feed_id >= 0:
+    try:
         feeds = g.db.query(Feed)
         feed = feeds.filter(Feed.id == feed_id).one()
         entries = g.db.query(Entry).filter(Entry.feed_id == feed_id).all()
-    else:
-        feeds = []
-        entries = []
-    return render_template('show_feeds.html',
-                           feed=feed, feeds=feeds, entries=entries)
+        return render_template('show_feeds.html',
+                               feed=feed, feeds=feeds, entries=entries)
+    # Not sure if this could be better handled elsewhere.
+    except sqlalchemy.orm.exc.NoResultFound as e:
+        return page_not_found(e)
+
+@feed_view.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 
 @feed_view.route('/refresh/<feed_id>')
