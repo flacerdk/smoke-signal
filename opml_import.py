0ec2c9140f57c327c106e4915ba6fd122314ea1a
Felipe Lacerda <fegolac@gmail.com>
2016-10-18 21:15:26 +0200

    fix opml_import.py to reflect recent changes in DB handling

--- opml_import.py
+++ opml_import.py
@@ -1,12 +1,8 @@
-from smoke_signal.database.models import Feed, Base
-from smoke_signal.db import init
-from config import DATABASE_PATH
+from flask import g
+from smoke_signal.database.models import Feed
+from smoke_signal import app, init_db
 import xml.etree.ElementTree as etree
 import sys
-from sqlalchemy.orm import sessionmaker
-
-engine = init(DATABASE_PATH)
-Session = sessionmaker(bind=engine)
 
 
 def opml_to_dict(filename):
@@ -17,10 +13,12 @@ def opml_to_dict(filename):
 
 
 def add_feed_list(feeds):
-    for f in feeds:
-        feed = Feed(f['title'], f['url'])
-        session = Session()
-        session.add(feed)
+    with app.app_context():
+        init_db()
+        session = g.db
+        for f in feeds:
+            feed = Feed(f['title'], f['url'])
+            session.add(feed)
         session.commit()
 
 
@@ -30,7 +28,7 @@ def opml_import(filename):
 
 
 def create_db_from_opml(filename):
-    Base.metadata.create_all(engine)
+    init_db()
     opml_import(filename)
 
 if __name__ == "__main__":
