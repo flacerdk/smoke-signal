46614ee9cab5e154c3483454611ffb91ac2aac83
Felipe Lacerda <fegolac@gmail.com>
2016-10-25 13:21:10 +0200

    Use let and const instead of var

--- smoke_signal/react/static/scripts/jsx/ajax_wrapper.js
+++ smoke_signal/react/static/scripts/jsx/ajax_wrapper.js
@@ -1,10 +1,10 @@
 module.exports = {
   getRequest: (url, callback) => {
-    var request = new XMLHttpRequest();
+    let request = new XMLHttpRequest();
     request.open('GET', url, true);
     request.onload = () => {
       if (request.status >= 200 && request.status < 400) {
-        var data = JSON.parse(request.responseText);
+        const data = JSON.parse(request.responseText);
         callback(data);
       } else {
         console.error(url, request.responseText);
@@ -17,13 +17,13 @@ module.exports = {
   },
 
   postJSONRequest: (url, data, callback) => {
-    var request = new XMLHttpRequest();
+    let request = new XMLHttpRequest();
     request.open('POST', url, true);
     request.setRequestHeader('Content-Type',
                              'application/json; charset=UTF-8');
     request.onload = () => {
       if (request.status >= 200 && request.status < 400) {
-        var data = JSON.parse(request.responseText);
+        const data = JSON.parse(request.responseText);
         callback(data);
       } else {
         console.error(url, request.responseText);
