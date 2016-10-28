f69e37e2400a8fad4386d17e9285c78aca4148c1
Felipe Lacerda <fegolac@gmail.com>
2016-10-28 15:26:52 +0800

    Move individual entries into their own component

---
+++ smoke_signal/react/static/scripts/jsx/entry.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class Entry extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    // Trusting that feedparser does proper sanitization here
+    let createMarkup =  () => { return { __html: this.props.text } };
+    return (
+      <div className="entry">
+        <a className="entry_title" href={this.props.url}>{this.props.title}</a>
+        <div dangerouslySetInnerHTML={createMarkup()} />
+      </div>
+    );
+  }
+}
+
+Entry.propTypes = {
+  title: React.PropTypes.string,
+  url: React.PropTypes.string,
+  text: React.PropTypes.string
+}
--- smoke_signal/react/static/scripts/jsx/entry_list.jsx
+++ smoke_signal/react/static/scripts/jsx/entry_list.jsx
@@ -1,3 +1,4 @@
+import Entry from './entry.jsx';
 import React from 'react';
 import { getRequest } from './ajax_wrapper.js';
 
@@ -6,28 +7,27 @@ export default class EntryList extends React.Component {
     super();
 
     this.state = {
+      feed_id: 0,
       entries: [],
     };
-
   }
 
   componentWillReceiveProps(nextProps) {
-    getRequest('/feeds/' + nextProps.params.id)
-      .then(entries => {
-          this.setState({entries: entries});
-        })
-      .catch(ex => console.log("Couldn't load feed: " + ex.message));
+    if (nextProps.params && nextProps.params.id &&
+        nextProps.params.id != this.state.feed_id) {
+      getRequest('/feeds/' + nextProps.params.id)
+        .then(entries => { this.setState({entries: entries})})
+        .catch(ex => console.log("Couldn't load feed: " + ex.message));
+    }
   }
-
   render() {
     const entries = this.state.entries.map(entry => {
-      // Trusting that feedparser does proper sanitization here
-      function createMarkup() { return { __html: entry.text } };
       return (
-        <div className="entry" key={entry.entry_id}>
-          <a className="entry_title" href={entry.url}>{entry.title}</a>
-          <div dangerouslySetInnerHTML={createMarkup()} />
-        </div>
+          <Entry
+              title={entry.title}
+              url={entry.url}
+              text={entry.text}
+              key={entry.entry_id} />
       );
     });
     return (
