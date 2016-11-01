b186291cf74593184b375b72bcda10c9bb409df5
Felipe Lacerda <fegolac@gmail.com>
2016-11-01 21:18:40 +0800

    Disable shortcuts until I figure out how to do that properly.

--- js/app.jsx
+++ js/app.jsx
@@ -2,11 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import FeedPage from './components/feed_page.jsx'
 import FeedReaderActions from './actions/feed_reader_actions.js'
-import { bindShortcuts } from './shortcuts/shortcuts.js'
 
 FeedReaderActions.refreshFeedList();
-
-document.onkeypress = bindShortcuts;
 
 ReactDOM.render((
   <FeedPage />
