07aa0f82328781c00926842a7f440b3cc928ac5a
Felipe Lacerda <fegolac@gmail.com>
2016-11-03 10:30:24 +0800

    Adapt FeedList to the Airbnb style

--- js/components/feed_list.jsx
+++ js/components/feed_list.jsx
@@ -1,31 +1,26 @@
 import React from 'react'
 import FeedReaderActions from '../actions/feed_reader_actions'
 
-export default class FeedList extends React.Component {
-  handleFeedClicked(feedId) {
-    FeedReaderActions.fetchFeedEntries(feedId)
-  }
+const handleFeedClicked = feedId => FeedReaderActions.fetchFeedEntries(feedId)
 
-  render() {
-    const feeds = this.props.feeds.map(feed => {
-      const link = "#/feeds/" + feed.id
-      let onClick = this.handleFeedClicked.bind(null, feed.id)
-      return (
-        <li className="feed" key={feed.id}>
-          <a className="feed" href={link} onClick={onClick}>{feed.title}</a>
-        </li>
-      )
-    })
-
+const FeedList = (props) => {
+  const feeds = props.feeds.map((feed) => {
+    const link = `#/feeds/${feed.id}`
+    const onClick = handleFeedClicked.bind(null, feed.id)
     return (
-      <div id="feeds">
-        <ul className="feed_list">{feeds}</ul>
-      </div>
+      <li className="feed" key={feed.id}>
+        <a className="feed" href={link} onClick={onClick}>{feed.title}</a>
+      </li>
     )
-  }
+  })
+
+  return (
+    <div id="feeds">
+      <ul className="feed_list">{feeds}</ul>
+    </div>
+  )
 }
 
 FeedList.propTypes = {
-  feeds: React.PropTypes.array,
-  onClick: React.PropTypes.func
+  feeds: React.PropTypes.arrayOf(React.PropTypes.object),
 }
