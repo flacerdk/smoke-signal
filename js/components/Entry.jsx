9991ce059c9afef595134875bb2f1f7fd129aeef
Felipe Lacerda <fegolac@gmail.com>
2016-11-04 14:49:36 +0800

    Reflect read status change in the UI

--- js/components/Entry.jsx
+++ js/components/Entry.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Entry = (props) => {
   // Trusting that feedparser does proper sanitization here
   const createMarkup = () => ({ __html: props.entry.text })
+  const className = props.entry.read ? "entry read" : "entry unread"
   return (
-    <div className="entry">
+    <div className={className}>
       <a className="entry_title" href={props.entry.url}>{props.entry.title}</a>
       <div dangerouslySetInnerHTML={createMarkup()} />
     </div>
@@ -16,6 +17,7 @@ Entry.propTypes = {
     title: React.PropTypes.string,
     url: React.PropTypes.string,
     text: React.PropTypes.string,
+    read: React.PropTypes.bool,
   }),
 }
 
