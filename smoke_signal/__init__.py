0b28fe44514969470db926c6f38615a8a5478bf6
Felipe Lacerda <fegolac@gmail.com>
2016-11-13 15:08:36 +0100

    Remove the no-JS version from the app
    I haven't looked into it for a long while.

--- smoke_signal/__init__.py
+++ smoke_signal/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, g
 from .main.views import main
-from .nojs.views import nojs
 from sqlalchemy import create_engine
 from smoke_signal.database.models import Base
 from sqlalchemy.orm import sessionmaker
@@ -9,7 +8,6 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_object("config")
 app.config.from_pyfile("config.py")
 app.register_blueprint(main)
-app.register_blueprint(nojs)
 
 
 @app.before_request
