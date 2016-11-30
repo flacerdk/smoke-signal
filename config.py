5c3b8a15118180980aa7747dd23ba5413bbb32ca
Felipe Lacerda <fegolac@gmail.com>
2016-11-30 19:12:06 +0100

    Update default config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 DEBUG = False
 SQLALCHEMY_ECHO = False
-DATABASE_PATH = "sqlite:///server/database/posts.db"
+DATABASE_PATH = "database/posts.db"
 USER_NAME = 'user name'
 PASSWORD = 'password'
 SECRET_KEY = 'secret key'
